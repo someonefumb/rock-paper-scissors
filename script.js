6bcd5c8950203c33fd4f7c353a5ebe7ad46662cd
someone_fumb <someonefumb@gmail.com>
2023-07-09 12:53:12 -0400

    Implement RPS game with scores

--- script.js
+++ script.js
@@ -20,21 +20,45 @@ function playRound () {
 
     switch(true) {
         case playerChoice === computerChoice:
+            playerScore++;
+            computerScore++;
             return alert('tie');
         case playerChoice === 'rock' && computerChoice === 'paper':
+            computerScore++;
             return alert('you lost, paper beats rock');
         case playerChoice === 'rock' && computerChoice === 'scissor':
+            playerScore++;
             return alert('you win, rock beats scissor');
         case playerChoice === 'paper' && computerChoice === 'rock':
+            playerScore++;
             return alert('you win, paper beats rock');
         case playerChoice === 'paper' && computerChoice === 'scissor':
+            computerScore++;
             return alert('you lost, scissor beats paper');
         case playerChoice === 'scissor' && computerChoice === 'rock':
+            computerScore++;
             return alert('you lost, rock beats scissor');
         case playerChoice === 'scissor' && computerChoice === 'paper':
+            playerScore++;
             return alert('you win, scissor beats paper');
     }
 
 }
 
-playRound();
+function game() {
+    for(i = 0; i < 5; i++) {
+        playRound();
+        console.log('Player Score:' + playerScore);
+        console.log('Computer Score:' + computerScore)
+    }
+
+    if (playerScore > computerScore) {
+        return alert('You won')
+    } else if (playerScore < computerScore) {
+        return alert('You lost')
+    } else {
+        return alert('Tied');
+    }
+}
+
+game();
