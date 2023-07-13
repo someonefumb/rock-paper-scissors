6f35240d1b332af974201a1defaa38d4749c0427
someone_fumb <someonefumb@gmail.com>
2023-07-13 00:02:02 -0400

    Add rps results to ui

--- script.js
+++ script.js
@@ -6,24 +6,25 @@ let computerScore = 0;
 let computer = ['rock', 'paper', 'scissor']
 let computerChoice = computer[Math.floor(Math.random() * 3)];
 
+// Result
+const result = document.querySelector('.rpsResult');
+
+// Plays a round of rock paper scissor
 function playRound() {
     if(playerChoice === computerChoice) {
-        playerScore++;
-        computerScore++;
-        return alert('tie');
+        result.textContent = 'You tied!';
     } else if (playerChoice === 'rock' && computerChoice === 'scissor'
         || playerChoice === 'paper' && computerChoice === 'rock' 
         || playerChoice === 'scissor' && computerChoice === 'paper') {
             playerScore++;
-            return alert(`you win, ${playerChoice} beats ${computerChoice}`);
-    } else if (playerChoice === 'rock' && computerChoice === 'paper'
-        || playerChoice === 'paper' && computerChoice === 'scissor' 
-        || playerChoice === 'scissor' && computerChoice === 'rock') {
+            result.textContent = `You won, ${playerChoice} beats ${computerChoice}!`;
+    } else {
             computerScore++;
-            return alert(`you lose, ${computerChoice} beats ${playerChoice}`);
+            result.textContent = `You lost, ${computerChoice} beats ${playerChoice}!`;
     }
 }
 
+// Buttons
 const buttons = document.querySelectorAll('.container button');
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
@@ -41,4 +42,5 @@ buttons.forEach((button) => {
     });
 });
 
+const game = document.querySelector('.gameStart');
 
